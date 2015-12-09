1175d043faaa1e421f6f8d60723433575c1b3a42
Adam Dry <adamdryit@gmail.com>
2015-12-09 09:54:28 +0000

    Website files

---
+++ js/bbkScripts.js
@@ -0,0 +1,35 @@
+function fixFooter() {
+	var winWidth = $(window).width();
+	var outterDivWidth = $('.outterDiv').width();
+	
+	if (outterDivWidth > winWidth) {
+		$('.footerDiv').width(outterDivWidth);
+	}
+	else {
+		$('.footerDiv').width(winWidth);
+	}
+}
+function fixMenu() {
+
+	// make the padding in the menu items evenly divided based on width
+	var outterDivWidth = $('.outterDiv').width();
+	var totalLisWidth = 0;
+	var itemCount = 0;
+	
+	$('#cssmenu ul li').each(function(index) {
+		totalLisWidth += $(this).width();
+		itemCount++;
+	});
+	
+	var difference = outterDivWidth - totalLisWidth;
+	
+	if (0 !== difference) {
+		
+		var amountPerLiToAlter = difference / itemCount;
+		var existingPadding = (parseInt($('#cssmenu li a').css('padding-left')) * 2);
+		var newPadding = existingPadding + amountPerLiToAlter;
+	
+		$('#cssmenu li a').css('padding-left', (newPadding / 2) + 'px');
+		$('#cssmenu li a').css('padding-right', (newPadding / 2) + 'px');
+	}
+}
